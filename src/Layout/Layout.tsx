2367d61d7a169e4de4799055daef696bd12348d5
Zilba26 <basile.lebaudy@gmail.com>
2023-11-13 10:26:40 +0100

    cookies added

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -2,17 +2,25 @@ import React, { FC } from 'react';
 import './Layout.css';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import { useCookies } from 'react-cookie';
 
 interface LayoutProps extends React.PropsWithChildren {}
 
-const Layout: FC<LayoutProps> = (props: LayoutProps) => (
-  <div className="root">
-    <Header />
-    <main>
-      {props.children}
-    </main>
-    <Footer />
-  </div>
-);
+const Layout: FC<LayoutProps> = (props: LayoutProps) => {
+  const [cookies, setCookie, removeCookie] = useCookies(['cookie-name']);
+
+  return(
+    <div className="root">
+      <Header />
+      <main>
+        <button onClick={() => setCookie('cookie-name', 'cookie-value')}>Set cookie</button>
+        <button onClick={() => removeCookie('cookie-name')}>Remove cookie</button>
+        <p>Cookie: {cookies['cookie-name']}</p>
+        {props.children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
 export default Layout;
